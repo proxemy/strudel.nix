e9067bc999a1a9545656ca059381040f325701f3
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-12-28 12:09:32 +0100

    Fixed strudel input and added test devShell.

--- flake.nix
+++ flake.nix
@@ -9,10 +9,25 @@
 
     strudel = {
       url = "https://codeberg.org/uzu/strudel";
+      type = "git";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, strudel }: {
+  outputs = { self, nixpkgs, strudel }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs-slim
+        pnpm
+      ];
+      shellHook = ''
+        pnpm --version
+      '';
+    };
   };
 }
